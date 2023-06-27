5aa3167c5f218da3cb82b6ba56e1eec45e808392
szkertesz <szabolcs.kertesz@gmail.com>
2023-06-27 12:50:27 +0200

    Implement MUI theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { StyledEngineProvider } from '@mui/material/styles'
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from '@mui/material/styles'
 import App from './App.tsx'
 import '@fontsource/roboto/300.css'
 import '@fontsource/roboto/400.css'
@@ -7,8 +13,25 @@ import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/700.css'
 import './index.css'
 
+let theme = createTheme({
+  palette: {
+    // maximize the contrast between
+    // the background and the text.
+    contrastThreshold: 4.5,
+  },
+})
+
+theme = responsiveFontSizes(theme)
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* change the CSS injection order to give precedence our custom styles over MUI
+     https://mui.com/material-ui/guides/interoperability/#css-injection-order
+     */}
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </StyledEngineProvider>
   </React.StrictMode>
 )
